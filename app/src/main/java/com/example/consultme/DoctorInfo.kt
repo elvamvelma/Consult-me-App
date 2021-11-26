da8162358643fa74b5dd59003899167856be13f5
elvamvelma <elvamvelma@gmail.com>
2021-11-26 13:21:02 +0300

    first commit

---
+++ app/src/main/java/com/example/consultme/DoctorInfo.kt
@@ -0,0 +1,11 @@
+package com.example.consultme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DoctorInfo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_doctor_info)
+    }
+}
